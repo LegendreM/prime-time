6b026c1c6ee8fafa46c2854af036d6ae14ff2310
mlegendre <mlegendre@vente-privee.com>
2019-02-03 17:41:43 +0100

    Add consumer

--- prime-worker/src/main.rs
+++ prime-worker/src/main.rs
@@ -1,3 +1,35 @@
+extern crate amqp;
+
+use amqp::{Session, Basic};
+use std::{thread, time};
+
+fn process() -> String {
+    "HELLO !".to_string()
+}
+
+fn conn_loop() {
+    let mut session = match Session::open_url("amqp://rabbit") {
+        Ok(session) => session,
+        Err(error) => {
+            println!("Can't create session: {:?}", error);
+            return ;
+        }
+    };
+
+    let mut channel = session.open_channel(1).ok().expect("Can't open channel!");
+    let queue_name = "task";
+
+    for get_result in channel.basic_get(queue_name, false) {
+        println!("Received: {:?}", String::from_utf8_lossy(&get_result.body));
+        get_result.ack();
+        println!("Process: {:?}", process());
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        conn_loop();
+        // wait 3sec before trying to reconnect
+        thread::sleep(time::Duration::from_millis(3000));
+    }
 }
